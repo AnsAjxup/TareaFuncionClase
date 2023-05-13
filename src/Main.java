65b23577d6bfde4bd19dde91f24fb768ea94eeea
AnsAjxup <aajxuph@miumg.edu.gt>
2023-05-13 12:01:58 -0600

    La funcion de suma ha sido creada

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        int resultadoSuma = funcionClase.suma(4,4, 4);
+
+        System.out.println("el total de la suma es= " + resultadoSuma);
+
+    }
+}
---
+++ src/funcionClase.java
@@ -0,0 +1,5 @@
+public class funcionClase {
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
